996aecf04bc38fc1205b03d3d6450c7faff9dd84
HenryWashuHe <he.h@wustl.edu>
2025-05-17 14:33:31 +0800

    finished

---
+++ script.js
@@ -0,0 +1,26 @@
+function showContactModal() {
+    const modal = document.getElementById("contactModal");
+    if (modal) {
+      modal.style.display = "flex";
+    }
+  }
+  
+  function hideContactModal() {
+    const modal = document.getElementById("contactModal");
+    if (modal) {
+      modal.style.display = "none";
+    }
+  }
+  function showLetter() {
+    const modal = document.getElementById("letterModal");
+    if (modal) {
+      modal.style.display = "flex";
+    }
+  }
+  
+  function hideLetter() {
+    const modal = document.getElementById("letterModal");
+    if (modal) {
+      modal.style.display = "none";
+    }
+  }
